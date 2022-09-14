d206c46a46931ee0b44ab16b0e2e2fed1fdf5d18
oracle123 <lington12@gmail.com>
2022-09-14 07:38:10 -0700

    task13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,7 @@ void print_times_table(int n)
 {
 	int row, column, product;
 
+
 	if (n >= 0 && n < 15)
 	{
 		for (row = 0; row <= n; row++)
@@ -26,9 +27,9 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(product % 10 + '0');
-					{
-						else if (product >= 10 && product < 100)
-					}
+				}
+				else if (product >= 10 && product < 100)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
