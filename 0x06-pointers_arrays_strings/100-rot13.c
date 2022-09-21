25b11eb3d814e7735ea4ec36da80670186e9e043
oracle123 <lington12@gmail.com>
2022-09-21 11:51:06 -0700

    100-rot13.c r

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-*rot13 - encodes a string into rot13
-*@s: string to encode
-*Return: address of s
+* *rot13 - encodes a string into rot13
+* *@s: string to encode
+* *Return: address of s
 */
 char *rot13(char *s)
 {
