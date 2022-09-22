30cca8d556378dbc7130b289d4cdc223623833f3
oracle123 <lington12@gmail.com>
2022-09-22 08:54:40 -0700

    task last

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
-*print_number - prints an integer.
-*@n: integer to be printed;
-*Return: Always 0
+*print_number - Prints any integer with putchar
+*@n: Number to prints
+*Return: Nothing
 */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		n *= -1;
 	}
-	if (n1 / 10)
-		print_number(n1 / 10);
-	
-	_putchar((n1 % 10) + '0');
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
