2c46114a11c5155d1450f354307a3153f450185e
oracle123 <lington12@gmail.com>
2022-09-26 07:36:23 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -19,21 +19,24 @@ unsigned int _strspn(char *s, char *accept)
 	i = 0;
 	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
-		 j = 0;
-		 while (str2[j] != '\0') /*Declaring WHILE *accept*/
-		 {
-			 if (str2[j] == str1[i]) /*Evaluate condition*/
-			 {
-				 count++; /*count number*/
-				 break;
-			 }
-			 j++;    /*add j+1*/
-		 }
-		 if (s[i] != accept[j]) /*If aren't equals*/
-		 {
-			 break;
-		 }
-		 i++; /*add x+1*/
+		j = 0;
+		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		{
+			if (str2[j] == str1[i]) /*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+
+			}
+
+			j++;    /*add j+1*/
+		}
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
 	}
 	return (count); /*return the value of count*/
 }
