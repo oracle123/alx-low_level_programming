1d03dcc493504ac995e25a2ea33b8be2df21d47a
oracle123 <lington12@gmail.com>
2022-10-10 15:13:03 -0700

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,33 +16,34 @@ struct dog *n_dog = NULL;
 
 lenN = 0;
 while (name[lenN] != '\0')
-	lenN++;
+
+lenN++;
 lenO = 0;
 while (owner[lenO] != '\0')
-	lenO++;
+lenO++;
 
 n_dog = malloc(sizeof(struct dog));
 if (n_dog == NULL)
 {
-	free(n_dog);
-	return (NULL);
+free(n_dog);
+return (NULL);
 }
 n_dog->name = malloc(lenN + 1);
 if (n_dog->name == NULL)
 {
-	free(n_dog->name);
-	free(n_dog);
-	return (NULL);
+free(n_dog->name);
+free(n_dog);
+return (NULL);
 }
+
 n_dog->owner = malloc(lenO + 1);
 if (n_dog->owner == NULL)
 {
-	free(n_dog->name);
-	free(n_dog->owner);
-	free(n_dog);
-	return (NULL);
+free(n_dog->name);
+free(n_dog->owner);
+free(n_dog);
+return (NULL);
 }
-
 for (i = 0; i <= lenN; i++)
 n_dog->name[i] = name[i];
 for (i = 0; i <= lenO; i++)
