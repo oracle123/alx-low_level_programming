9a14543def13621934b3a3cb2fd97bd27c2f5043
oracle123 <lington12@gmail.com>
2022-10-20 16:29:11 -0700

    correction

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,12 +14,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	size_t n;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
+
 	for (n = 0; str[n]; n++)
 		;
+
 	new_node->len = n;
 	new_node->next = NULL;
 	current_node = *head;
